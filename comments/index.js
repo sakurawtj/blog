f4506b9fc8f7a96a0fb0870211a3744412ffc611
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-10-16 10:15:12 -0700

    add moderation functionality

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,15 +14,49 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({id: commentId, content});
+    comments.push({id: commentId, content, status: "pending"});
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId, 
+            content,
+            postId: req.params.id,
+            status:'pending'
+        }
+    });
     commentsByPostId[req.params.id] = comments;
     res.status(201).send(comments);
 });
+app.post('/events', async (req, res) => {
+    console.log('Event Received:', req.body.type);
+    const {type, data} = req.body;
+    if(type === 'CommentModerated') {
+        const{postId, id, content, status} = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+    
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                content,
+                status,
+                postId,
+                
+            }
+        });
+    }
+    res.send({});
+})
+
 
 app.listen(4001, () => {
     console.log('listening on 4001')
