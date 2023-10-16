90357d06cc1eef305551046b86debd36ef9a4ff3
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-10-16 10:16:03 -0700

    add moderation functionality

---
+++ client/src/CommentList.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+export default ({comments}) => {
+
+    const renderedComments = Object.values(comments).map(comment => {
+        let content;
+        if(comment.status ==='approved') {
+            content = comment.content;
+        }
+        if (comment.status === 'pending') {
+            content = "This comment is awaiting moderation";
+        }
+        if (comment.status === 'rejected') {
+            content = "This comment is rejected";
+        }
+        return <li key={comment.id}>{content}</li>
+    });
+    return <ul>{renderedComments}</ul>
+
+}
