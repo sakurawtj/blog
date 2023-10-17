d37ac0198c850f2060881cc63bae1b542849cb0d
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-10-17 12:49:56 -0700

    event sync

--- query/index.js
+++ query/index.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
-    if(type === 'PostCreated') {
-        const {id, title} = data;
-        posts[id] = {id, title, comments: []};
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
     }
     if (type === 'CommentCreated') {
-        const {id, content, postId, status} = data;
+        const { id, content, postId, status } = data;
         const post = posts[postId];
         post.comments.push({ id, content, status });
     }
@@ -32,10 +27,29 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
-    console.log(posts);
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+    handleEvent(type, data);
     res.send({});
 });
 
-app.listen(4002, () => {
-    console.log('Listening on 4002')
+app.listen(4002, async () => {
+    console.log('Listening on 4002');
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 })
